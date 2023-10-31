c9fbc94f368d1bec27069f2fd3d9e186b240db0f
Robin <robinkolk100@outlook.com>
2023-10-31 22:27:09 +0100

    done

---
+++ kerncentrale/kerncentrale/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Centrale centrale = new Centrale();
+
+        System.out.println(centrale.run(0.1, 1) + "kWh");
+    }
+}
---
+++ kerncentrale/kerncentrale/src/Splijtstaaf.java
@@ -0,0 +1,8 @@
+interface Splijtstaaf {
+
+    public double split(double tijd, double temp) throws MeltdownException;
+    public double getStoom();
+    public double getRestWarmte();
+    public double getRestpercentage();
+
+}
